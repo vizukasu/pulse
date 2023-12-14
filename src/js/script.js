88ea529e7a906326131fb56c85f389020bf96692
vizukasu <skemetrtalgat@gmail.com>
2023-12-14 17:42:33 +0600

    jq 2

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../src/icons/carousel/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../src/icons/carousel/rigth.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/rigth.png"></button>',
         responsive: [
             {
                 breakpoint: 992,
